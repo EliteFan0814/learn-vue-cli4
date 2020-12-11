db6691606751370fc5b236bbc47b83d8a9da5c89
EliteFan <fan552426811@outlook.com>
2020-12-11 08:05:23 +0800

    fpc add

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <input type="file">
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+
+}  
+</style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="layout">
+    <input type="file" ref="imgUpload" name="pic" accept="image/*" @change="handleChange">
+    <div>
+      <el-button type="primary" size="mini" @click="handleUpload">上传图片</el-button>
+      <span class="pic-name">dfefreji.jpg{{imgName}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fileInfo: undefined,
+      imgName:undefined,
+      imgFile:undefined
+    }
+  },
+  methods: {
+    handleUpload() {
+      this.$refs.imgUpload.click()
+    },
+    handleChange(event) {
+      const file = event.target.files
+      this.imgName = file[0].name
+      this.imgFile = file
+      console.log(event.target.files)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout{
+input{
+  display:none;
+}
+.pic-name{
+  padding-left: 10px;
+}
+}  
+</style>
